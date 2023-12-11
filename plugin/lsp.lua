c95ee9bb77639737f308d9bb25579da324938b5e
Quinn Strahl <qstrahl@gmail.com>
2023-12-11 14:47:59 -0500

    Fix spacing in lsp.lua

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -199,17 +199,17 @@ local function on_lsp_attach (ev)
   -- See `:help vim.lsp.*` for documentation on any of the below functions
 
   -- builtin lsp functions
-  set_buf_keymap('n', 'gD',           vim.lsp.buf.declaration             )
-  set_buf_keymap('n', 'gd',           vim.lsp.buf.definition              )
-  set_buf_keymap('n', '<Leader>D',     vim.lsp.buf.type_definition         )
-  set_buf_keymap('n', 'gI',           vim.lsp.buf.implementation          )
-  set_buf_keymap('n', 'K',            vim.lsp.buf.hover                   )
-  set_buf_keymap('n', '<C-k>',        vim.lsp.buf.signature_help          )
-  set_buf_keymap('n', '<Leader>a',     vim.lsp.buf.code_action             )
-  set_buf_keymap('n', 'gr',           vim.lsp.buf.references              )
-  set_buf_keymap('n', '<Leader>n',     vim.lsp.buf.rename                  )
-  set_buf_keymap('n', '<Leader>wa',    vim.lsp.buf.add_workspace_folder    )
-  set_buf_keymap('n', '<Leader>wr',    vim.lsp.buf.remove_workspace_folder )
+  set_buf_keymap('n', 'gD',             vim.lsp.buf.declaration                 )
+  set_buf_keymap('n', 'gd',             vim.lsp.buf.definition                  )
+  set_buf_keymap('n', '<Leader>D',      vim.lsp.buf.type_definition             )
+  set_buf_keymap('n', 'gI',             vim.lsp.buf.implementation              )
+  set_buf_keymap('n', 'K',              vim.lsp.buf.hover                       )
+  set_buf_keymap('n', '<C-k>',          vim.lsp.buf.signature_help              )
+  set_buf_keymap('n', '<Leader>a',      vim.lsp.buf.code_action                 )
+  set_buf_keymap('n', 'gr',             vim.lsp.buf.references                  )
+  set_buf_keymap('n', '<Leader>n',      vim.lsp.buf.rename                      )
+  set_buf_keymap('n', '<Leader>wa',     vim.lsp.buf.add_workspace_folder        )
+  set_buf_keymap('n', '<Leader>wr',     vim.lsp.buf.remove_workspace_folder     )
 
   -- custom lsp functions
   set_buf_keymap('n', '<Leader>wl',        list_workspace_folders  )
