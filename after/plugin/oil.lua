411d71149707c4f8cf50d2346b29151587c795da
Quinn Strahl <qstrahl@gmail.com>
2023-12-15 11:48:12 -0500

    Add custom oil buffer keymaps

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,6 +1,12 @@
 require('oil').setup {
   delete_to_trash = true,
   skip_confirm_for_simple_edits = true,
+  keymaps = {
+    ['<C-->'] = 'actions.select_split',
+    ['<C-\\>'] = 'actions.select_vsplit',
+    ['<C-CR>'] = 'actions.select_tab',
+    ['_'] = false,
+  },
   view_options = {
     show_hidden = true,
     is_always_hidden = function(name)
